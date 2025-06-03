e9a147330c42cee3fe7425f5ee8c37c0eb558bcc
Dmitry Kniazev <dmi.knyazev@tbank.ru>
2025-06-03 19:29:59 +0300

    Initial Commit

---
+++ TestCICD/TestCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDApp.swift
+//  TestCICD
+//
+//  Created by Dmitriy Knyazev on 3.06.25.
+//
+
+import SwiftUI
+
+@main
+struct TestCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
